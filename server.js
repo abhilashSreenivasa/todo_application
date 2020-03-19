0d99373730d0b38ea75914b2fc8a18d90ad70b98
abhilashSreenivasa <abhilash.sreenivasa@gmail.com>
2020-03-19 15:41:38 +0530

    register page

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ if(process.env.NODE_ENV !== 'production'){
 const express=require('express')
 const app=express()
 const expressLayouts=require('express-ejs-layouts')
+const bodyParser=require('body-parser')
 
 const indexRouter=require('./routes/index')
+const loginRouter=require('./routes/login')
+const registerRouter=require('./routes/register')
 
 
 app.set('view engine','ejs')
@@ -13,8 +16,11 @@ app.set('views',__dirname+'/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb',extended:'false'}))
 
 app.use('/',indexRouter)
+app.use('/login',loginRouter)
+app.use('/register',registerRouter)
 
 
 
